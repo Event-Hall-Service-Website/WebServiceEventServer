516dd56687285298bcd0541f08a0bf8771849b77
techemmydev <gabrielemmanuelmary12345@gmail.com>
2025-03-15 03:05:17 +0100

    added some fetures

--- Routes/auth.route.js
+++ Routes/auth.route.js
@@ -4,13 +4,18 @@ import { getAllHalls } from "../controllers/hallController.js";
 import { postHall } from "../controllers/hallController.js";
 import { submitContactForm } from "../controllers/submitContactForm.js";
 import { Subscribers } from "../controllers/SubsscribersController.js";
-
+import {
+  getAllFeedbacks,
+  postFeedback,
+} from "../controllers/FeedbacController.js";
 const router = express.Router();
 router.post("/bookings", BookForAnEvent); // GET /api/booking
 router.get("/gethalls", getAllHalls); // GET /api/gethalls
 router.post("/posthall", postHall); // POST /api/posthall
 router.post("/contact", submitContactForm);
 router.post("/subscribe", Subscribers);
+router.get("/feedbacks", getAllFeedbacks);
+router.post("/feedback", postFeedback);
 
 export default router;
 // In the above code, we have defined three routes:
